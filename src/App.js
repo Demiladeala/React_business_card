bb9e342b3b55099f62166d1922877a558e3ebea7
Oluwademilade Ala <85693382+Demiladeala@users.noreply.github.com>
2023-01-09 11:40:34 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import './App.css';
+import React from 'react';
+import About from '../src/components/About';
+import Footer from '../src/components/Footer';
+import Info from '../src/components/Info';
+import Interest from '../src/components/Interest';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+export default function App(){
+    return(
+    <div className='container'>
+        <div className='card-container'>
+        <Info />
+        <div className='card-info'>
+        <About />
+        <Interest />
+        </div>
+        <Footer />
+        </div>
     </div>
-  );
+    )
 }
-
-export default App;
